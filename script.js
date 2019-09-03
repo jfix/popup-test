7c4c82c87726545fb3ebf7462342b3d804724fb5
Jakob Fix <jakob.fix@gmail.com>
2019-09-03 18:16:28 +0200

    Move functions outside of event listener

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
 window.addEventListener("load", () => {
   var popup = document.getElementById("popup");
 
-  // display popup and load iframe on demand
-  function showPopup() {
-    var iframe = document.querySelector("iframe");
-    var url = iframe.getAttribute("data-src");
-    iframe.setAttribute("src", url);
-    var popup = document.getElementById("popup");
-    popup.style.display = "block";
-  }
-
-  // hide popup
-  function dismissPopup() {
-    var popup = document.getElementById("popup");
-    popup.style.display = "none";
-  }
-
   // display popup after 3 seconds
   setTimeout(showPopup, 3000);
 
@@ -28,3 +13,18 @@ window.addEventListener("load", () => {
     }
   });
 });
+
+// display popup and load iframe on demand
+function showPopup() {
+  var iframe = document.querySelector("iframe");
+  var url = iframe.getAttribute("data-src");
+  iframe.setAttribute("src", url);
+  var popup = document.getElementById("popup");
+  popup.style.display = "block";
+}
+
+// hide popup
+function dismissPopup() {
+  var popup = document.getElementById("popup");
+  popup.style.display = "none";
+}
